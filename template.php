2fdefb38a7ed834a5a19f1f0fa6f559a48259764
Александр <75223388+Alexander1-cloud@users.noreply.github.com>
2026-07-22 17:36:27 +0500

    Update template.php

--- template.php
+++ template.php
@@ -1,13 +1,13 @@
 <?php defined('A') or die; /* This is Open Source script for site template */ ?>
 <html>
 <head>
-<?php /* My secret key is md5 string, you can use your own key or delete the meta tag with the name="secret key". */ ?>
 <meta charset='UTF-8'>
 <title>Сайт не существует.</title>
 <meta name="description" content="Сайт не существует." />
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <link media="all" href="/style.css?lastupdate=<?php echo filemtime('style.css')?>" rel="stylesheet">
 <link rel='canonical' href='<? echo ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' .$_SERVER['HTTP_HOST'];?>/' />
+<?php /* My secret key is md5 string, you can use your own key or delete the meta tag with the name="secret key". */ ?>
 <meta name="key" content="51351bccf6258f98c318f3a79e7ae903" />
 <?php /* This is an Open Source template for a disabled site */ ?>
 </head>
